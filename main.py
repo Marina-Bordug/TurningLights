cd76179dab4442ef8a8114c6541a876addfcbd71
ev_b <rainbowviola3@gmail.com>
2025-01-15 20:19:23 +0300

    created simple logic

--- main.py
+++ main.py
@@ -1,81 +1,224 @@
 import pygame
+import sys
+import os
 
-WHITE = pygame.Color("#ffffff")
-BLACK = pygame.Color("#000000")
-RED = pygame.Color(255, 0, 0)
-BLUE = pygame.Color(0, 0, 255)
-SIZE = WIDTH, HEIGHT = (600, 600)
-
-
-class TurningLights:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
-        pygame.display.set_caption("TurningLights")
-
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def render(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                cell_x = self.left + self.cell_size * x
-                cell_y = self.top + self.cell_size * y
-                rect = (cell_x, cell_y, self.cell_size, self.cell_size)
-                if self.board[y][x] == -2:
-                    pygame.draw.rect(screen, BLUE, rect)
-                    pygame.draw.rect(screen, WHITE, rect, 1)
-                else:
-                    if self.board[y][x] == 0:
-                        pygame.draw.rect(screen, WHITE, rect, 1)
-                    if self.board[y][x] == -1:
-                        pygame.draw.rect(screen, RED, rect)
-                        pygame.draw.rect(screen, WHITE, rect, 1)
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        if cell:
-            self.on_click(cell)
-        else:
-            pass
-
-    def get_cell(self, mouse_pos):
-        mouse_x, mouse_y = mouse_pos
-        x = (mouse_x - self.left) // self.cell_size
-        y = (mouse_y - self.top) // self.cell_size
-        if not (0 <= x < self.width and 0 <= y < self.height):
-            return None
-        else:
-            return x, y
-
-    def on_click(self, cell):
-        cell_x, cell_y = cell
-        if self.board[cell_y][cell_x] == -2:
-            self.board[cell_y][cell_x] = 0
-        else:
-            self.board[cell_y][cell_x] = self.board[cell_y][cell_x] - 1
+BLACK = pygame.Color(0, 0, 0)
+SIZE = WIDTH, HEIGHT = (500, 500)
+FPS = 60
 
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+# MENU -----------
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('bg.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, BLACK)
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_level(filename):
+    # filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+# отрисовка
+def generate_level(level):
+    new_player, x, y = None, None, None
+    playerx, playery = None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Box('wall', x, y)
+            elif level[y][x] == '&':
+                Tile('empty', x, y)
+                Lamp('object', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                playerx, playery = x, y
+    new_player = Player(playerx, playery)
+
+    return new_player, x, y
+
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png'),
+    'object': load_image('light-off.png'),
+    'object2': load_image('light-on.png')
+}
+
+player_image = load_image('mar.png')
+
+tile_width = tile_height = 50
+
+
+# class Camera:
+#     # зададим начальный сдвиг камеры
+#     def __init__(self):
+#         self.dx = 0
+#         self.dy = 0
+#
+#     # сдвинуть объект obj на смещение камеры
+#     def apply(self, obj):
+#         obj.rect.x += self.dx
+#         obj.rect.y += self.dy
+#
+#     # позиционировать камеру на объекте target
+#     def update(self, target):
+#         self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+#         self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(box_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Lamp(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(lamp_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def move(self, key):
+        delta_x, delta_y = 0, 0
+        d_1, d_2 = 0, 0
+        if key == pygame.K_UP:
+            delta_x, delta_y = 0, -tile_height
+            d_1, d_2 = 0, -1
+        if key == pygame.K_DOWN:
+            delta_x, delta_y = 0, tile_height
+            d_1, d_2 = 0, 1
+        if key == pygame.K_LEFT:
+            delta_x, delta_y = -tile_height, 0
+            d_1, d_2 = -1, 0
+        if key == pygame.K_RIGHT:
+            delta_x, delta_y = tile_height, 0
+            d_1, d_2 = 1, 0
+        self.rect = self.rect.move(delta_x, delta_y)
+
+        self.pos_x += d_1
+        self.pos_y += d_2
+        print(self.pos_x, self.pos_y)
+        if key == pygame.K_a:
+            print(1)
+            delta_x, delta_y = 0, -tile_height
+            d_1, d_2 = 0, -1
+            self.pos_x += d_1
+            self.pos_y += d_2
+            self.rect = self.rect.move(delta_x, delta_y)
+            if pygame.sprite.spritecollideany(self, lamp_group):
+                print(2)
+                print(self.pos_x, self.pos_y)
+                Tile('empty', self.pos_x, self.pos_y)
+                Lamp('object2', self.pos_x, self.pos_y)
+                self.rect = self.rect.move(-delta_x, -delta_y)
+            else:
+                self.rect = self.rect.move(-delta_x, -delta_y)
+            delta_x, delta_y = 0, 0
+        if pygame.sprite.spritecollideany(self, box_group):
+            self.rect = self.rect.move(-delta_x, -delta_y)
+            self.pos_x -= delta_x
+            self.pos_y -= delta_y
+        elif pygame.sprite.spritecollideany(self, lamp_group):
+            self.rect = self.rect.move(-delta_x, -delta_y)
+            self.pos_x -= delta_x
+            self.pos_y -= delta_y
+
+
+player = None
+# camera = Camera()
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 
-run = True
+all_sprites = pygame.sprite.Group()
+lamp_group = pygame.sprite.Group()
+box_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
-board = TurningLights(12, 12)
-board.set_view(0, 0, 50)
-while run:
-    screen.fill(BLACK)
+clock = pygame.time.Clock()
+player, level_x, level_y = generate_level(load_level('data/map.txt'))
+
+start_screen()
+# GAME ----------
+while True:
+    clock.tick(FPS)
     for event in pygame.event.get():
-        run = event.type != pygame.QUIT
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            board.get_click(event.pos)
-    board.render(screen)
-    pygame.display.update()
+        if event.type == pygame.QUIT: terminate()
+        if event.type == pygame.KEYDOWN:
+            player.move(event.key)
+    # camera.update(player)
+    for sprite in all_sprites:
+        pass
+        # camera.apply(sprite)
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    all_sprites.update()
+    pygame.display.flip()
 
-pygame.quit()
+# pygame.quit()
